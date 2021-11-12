193a271445d5d94e34c16b402d650788eafe4186
DevMeans <tavata.canales@gmail.com>
2021-11-11 19:36:26 -0500

    Se añadio el c/u/d falta listar *

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,5 @@
 
-const { response, request } = require("express")
+const { response, request, json } = require("express")
 const usuario = require("../models/usuario")
 
 const crearUsuario = async (req = request, res = response) => {
@@ -12,6 +12,45 @@ const crearUsuario = async (req = request, res = response) => {
         msg: 'Estas en el controlador crear usuario !!!!'
     })
 }
+
+const actualizarUsuaurio = (req = request, res = response) => {
+    const { id } = req.params
+    const { password, rol, estado, ...newBody } = req.body
+    const usuarioDB = await usuario.findByIdAndUpdate(id, newBody)
+    if (!usuarioDB) {
+        return res.status(494).json({
+            ok: false,
+            msg: `usuario con el id ${id} no existe`
+        })
+    }
+    return res.json({
+        ok: true,
+        msg: `usuario actualizado correctamente`
+    })
+
+}
+const actulziarEstadoUsuario = (req = request, res = response) => {
+    const { id } = req.params
+    const { estado } = req.body
+    const usuarioDB = await usuario.findByIdAndUpdate(id, estado)
+    if(!usuarioDB){
+        return res.status(404).json({
+            ok:false,
+            msg: `usuario con el id ${id} no existe`
+        })
+    }
+    return res.json({
+        ok:false,
+        msg: `usuario actualizado correctamente`
+    })
+}
+const listarUsuarios=(req = request, res = response)=>{
+    const { limite,desde } =req.params
+    
+}
+
 module.exports = {
-    crearUsuario
+    crearUsuario,
+    actualizarUsuaurio,
+    actulziarEstadoUsuario
 }
